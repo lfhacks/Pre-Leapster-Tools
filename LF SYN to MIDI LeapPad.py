33275ae344a438978224b53c9a9c543abfc71984
Dr. RNG <33909717+BLiNXthetimesweeperGOD@users.noreply.github.com>
2024-08-21 08:57:19 -0400

    Port back all MIDI conversion fixes from Leapster-Tools
    This update fixes tons of issues with conversions such as broken loop points and bad volume handling

--- LF SYN to MIDI LeapPad.py
+++ LF SYN to MIDI LeapPad.py
@@ -4,15 +4,13 @@ from tkinter import filedialog
 import mido
 import os
 
-# Set up the tkinter file dialog
 root = tk.Tk()
 root.withdraw()
 files = filedialog.askopenfilenames()
 root.destroy()
 currentTrack = 0
-# Create a new MIDI file with one track
 
-# Function to add MIDI messages to the track
+#Function to add MIDI messages to the track
 def add_midi_message(message_type, channel, data1, data2, delta_time):
     if message_type == 'program_change':
         track.append(mido.Message('program_change', program=data1, channel=channel, time=delta_time))
@@ -47,16 +45,14 @@ for file_path in files:
                 syn.seek(trackoffset)
                 currentSynTrack += 1
                 current_bend = 8192
-                #LeapPad only has 6 channels, making this pointless.
+                hasLoop = False
+                hasLoopEnd = False
                 if currentSynTrack == 9: #The bothersome "percussion" channel must be stopped. It has ruined countless lives and - oh, wait. I can just skip it.
                     currentSynTrack += 1
-                    print("Skipped that one track everyone hates. Yay!")
-
                 while True:
                     data = syn.read(1)
                     if len(data) == 0:
                         break
-                    #try:
                     if data[0] == 0x81: #PitchShift required.
                         add_midi_message('control_change', currentSynTrack, 108, 0, 0)
                     if data[0] == 0x82: #PitchShift not required.
@@ -69,7 +65,6 @@ for file_path in files:
                     
                     if data[0] == 0x88: #Set volume
                         volume = syn.read(1)[0]
-                        add_midi_message('control_change', currentSynTrack, 7, volume, 0)
                     
                     if data[0] == 0x89: #Program change
                         instrument = syn.read(1)[0]
@@ -80,10 +75,10 @@ for file_path in files:
                             add_midi_message('program_change', currentSynTrack, instrument, None, 0)
 
                     if data[0] == 0x8A: #Pitch bend
-                        bend = struct.unpack("<b", syn.read(1))[0]
+                        bend = struct.unpack("<B", syn.read(1))[0]
                         duration = syn.read(1)[0]
                         if duration == 0x80:
-                            duration -= 0x7F
+                            duration -= 0x80
                             duration_2 = syn.read(1)[0]
                             duration += duration_2
                         elif duration >= 0x81 and duration < 0xFF:
@@ -98,25 +93,28 @@ for file_path in files:
                                 duration = int(str("0x"+main+duration_2), 16)
                             else:
                                 duration = int(str("0x"+main+"0"+duration_2), 16)
-                        add_midi_message('pitch_wheel', currentSynTrack, bend, 127, duration)
+                        current_bend = -4096 + int(bend*32)
+                        #print(current_bend)
+                        #input()
+                        if current_bend > 8192 or current_bend < -8192:
+                            print(current_bend)
+                            current_bend =0
+                        add_midi_message('pitch_wheel', currentSynTrack, current_bend, 127, duration)
 
 
                     if data[0] == 0x8E: #Start of loop
                         loopCount = syn.read(1)[0]
-                        add_midi_message('control_change', currentSynTrack, 110, loopCount, 0)
+                        hasLoop = True #Loop points are now added after the previous note ends to ensure that they're timed right
                         
                     if data[0] == 0x8F: #End of loop
                         zero = syn.read(1)[0]
-                        add_midi_message('control_change', currentSynTrack, 111, zero, 0)
+                        hasLoopEnd = True #Loop points are now added after the previous note ends to ensure that they're timed right
                         
                     if data[0] in range(0x00, 0x7F):
                         note = data[0]
                         duration = syn.read(1)[0]
-                        if duration == 0x80:
-                            duration -= 0x7F
-                            duration_2 = syn.read(1)[0]
-                            duration += duration_2
-                        elif duration >= 0x81 and duration < 0xFF:
+                        current_bend = 0 #Reset the pitch bend for every note (mainly because figuring out how they work is still a work in progress)
+                        if duration >= 0x80 and duration < 0xFF:
                             duration -= 0x80
                             if duration <= 0xF:
                                 main = str(hex(duration))[2:3]
@@ -129,18 +127,24 @@ for file_path in files:
                             else:
                                 duration = int(str("0x"+main+"0"+duration_2), 16)
                         if lastDuration != -1:
-                            add_midi_message('note_off', currentSynTrack, lastNote, 127, lastDuration) #Now end the note
+                            add_midi_message('note_off', currentSynTrack, lastNote, volume, lastDuration) #Now end the note
+                            if hasLoop == True:
+                                add_midi_message('control_change', currentSynTrack, 110, loopCount, 0)
+                                hasLoop = False
                         if note >= 1:
-                            add_midi_message('note_on', currentSynTrack, note, 127, 0) #Start the note
+                            add_midi_message('note_on', currentSynTrack, note, volume, 0) #Start the note
                         
                         
                         lastDuration = duration
                         lastNote = note
 
                     if data[0] == 0xFF: #End of sequence
-                        add_midi_message('note_off', currentSynTrack, lastNote, 127, lastDuration) #Just added to ensure that the last note in a track actually ends
-                        add_midi_message('note_off', currentSynTrack, 0, 0, 200) #Add silence at the end so the last note doesn't end early in non-looping SYN files
+                        add_midi_message('note_off', currentSynTrack, lastNote, 127, lastDuration)
+                        if hasLoopEnd == True:
+                            add_midi_message('control_change', currentSynTrack, 111, zero, 0)
+                            hasLoopEnd = False
                         break
+            
 ##                    except:
 ##                        if syn.tell() == os.path.getsize(file_path):
 ##                            print("Oops!")
