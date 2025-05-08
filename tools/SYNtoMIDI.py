5263819550534a345dd7a63977610da75cfd69a2
Dr. RNG <33909717+BLiNXthetimesweeperGOD@users.noreply.github.com>
2025-05-08 16:49:23 -0400

    Pitch bends should be fixed now
    The issue that remained was timing-related. This fixes the timing issue, making them effectively perfect minus one very strange and specific case that needs to be looked into further.

--- tools/SYNtoMIDI.py
+++ tools/SYNtoMIDI.py
@@ -33,24 +33,27 @@ def decodeSYN(synFile, offsets, system, output_base_name):
         if system == "LeapPad":
             midi.set_pitch_bend_range(track, 7)
         if system == "Leapster":
-            midi.set_pitch_bend_range(track, 2)
+            midi.set_pitch_bend_range(track, 7)
         
         #Position at track start
         synFile.seek(offset)
         decoder = BE_BitReader(synFile)
-        
+        totalDurationSinceLastNote = 0
         #Decode track data
         while True:
+            
             storedBend = 0
             data = decoder.read(8) #Read 8 bits for the command check
-            
             if data in range(0, 0x7F): #Note
                 if noteIsOn == True:
                     noteIsOn = False
                     track.append(midi.create_note_off_message(storedNote, volume, time=storedDuration))
                     bend = 0
                     track.append(midi.create_pitch_bend_message(int(bend), time=0))
-                    
+                if storedNote !=  0:
+                    totalDurationSinceLastNote = 0
+                    track.append(midi.create_note_off_message(storedNote, volume, time=0))
+                    storedNote = 0
                 variableWidthBit = decoder.read(1)
                 if variableWidthBit == 0: #Duration is 127 ticks or lower
                     duration = decoder.read(7)
@@ -71,18 +74,19 @@ def decodeSYN(synFile, offsets, system, output_base_name):
                         storedDuration = duration
                     else:
                         track.append(midi.create_note_off_message(data, volume, time=duration))
+                totalDurationSinceLastNote+=duration
                         
             elif data == 0x81:
-                print("PitchShift Required.")
+                ""#print("PitchShift Required.")
                 
             elif data == 0x82:
-                print("PitchShift not Required.")
+                ""#print("PitchShift not Required.")
                 
             elif data == 0x83:
-                print("Reserve voice")
+                ""#print("Reserve voice")
                 
             elif data == 0x84:
-                print("Release voice")
+                ""#print("Release voice")
                 
             elif data == 0x88:
                 volume = decoder.read(8)
@@ -96,6 +100,12 @@ def decodeSYN(synFile, offsets, system, output_base_name):
                     track.append(midi.create_program_change_message(data))
                     
             elif data == 0x8A: #Pitch bend
+                if noteIsOn == True:
+                    noteIsOn = False
+                    track.append(midi.create_note_off_message(0, volume, time=storedDuration))
+                    storedDuration = 0
+                    bend = 0
+                    track.append(midi.create_pitch_bend_message(int(bend), time=0))
                 if system == "LeapPad":
                     bendData = (decoder.read(8)*64) #This literally maps to the source MIDIs near-perfectly in the output but is still bad...
                     if bendData != 0x3fc0:
@@ -111,7 +121,6 @@ def decodeSYN(synFile, offsets, system, output_base_name):
                         duration = decoder.read(15)
                         track.append(midi.create_pitch_bend_message(int(bend), time=0))
                         track.append(midi.create_note_off_message(0, volume, time=duration))
-                        
                 if system == "Leapster":
                     bendData = (decoder.read(8)*64)
                     if bendData != 0x3fc0:
@@ -127,11 +136,14 @@ def decodeSYN(synFile, offsets, system, output_base_name):
                         duration = decoder.read(15)
                         track.append(midi.create_pitch_bend_message(int(bend), time=0))
                         track.append(midi.create_note_off_message(0, volume, time=duration))
+                    totalDurationSinceLastNote+=duration
                     
             elif data == 0x8E: #Set loop count and loop start position
                 if noteIsOn == True:
                     noteIsOn = False
                     track.append(midi.create_note_off_message(storedNote, volume, time=storedDuration))
+                    bend = 0
+                    track.append(midi.create_pitch_bend_message(int(bend), time=0))
                     
                 loops = decoder.read(8)
                 track.append(midi.create_control_change_message(110, loops))
@@ -140,6 +152,8 @@ def decodeSYN(synFile, offsets, system, output_base_name):
                 if noteIsOn == True:
                     noteIsOn = False
                     track.append(midi.create_note_off_message(storedNote, volume, time=storedDuration))
+                    bend = 0
+                    track.append(midi.create_pitch_bend_message(int(bend), time=0))
                     
                 padding = decoder.read(8)
                 track.append(midi.create_control_change_message(111, 0))
@@ -149,7 +163,7 @@ def decodeSYN(synFile, offsets, system, output_base_name):
                     noteIsOn = False
                     track.append(midi.create_note_off_message(storedNote, volume, time=storedDuration))
                 break
-
+        
         #Save individual track to the WORKING directory
         track.append(midi.create_note_off_message(0, volume, time=128))
         trackFilename = f'track_{index:02d}.mid'
@@ -170,37 +184,37 @@ if not synFiles:
 else:
     #Process each selected SYN file
     for synPath in synFiles:
-        #try:
-        #Get base filename without extension for naming output files
-        input_filename = os.path.basename(synPath).split('.')[0]
-        print(f"\nProcessing SYN file: {synPath}")
-        
-        with open(synPath, "rb") as f:
-            trackOffsets = []
-            headCheck = f.read(2)
+        try:
+            #Get base filename without extension for naming output files
+            input_filename = os.path.basename(synPath).split('.')[0]
+            print(f"\nProcessing SYN file: {synPath}")
             
-            if headCheck != b'\x02\x00': #LeapPad SYN
-                f.seek(0)
-                trackOffsets = BE_multiunpack.ushort(f.read(12))
-                system = "LeapPad"
-            else: #Leapster SYN
-                trackCount = LE_unpack.ushort(f.read(2))
-                for track in range(trackCount):
-                    offset = LE_unpack.ushort(f.read(2))
-                    ID = BE_unpack.ushort(f.read(2))
-                    trackOffsets.append(offset)
-                system = "Leapster"
-            
-            #Filter out any zero offsets
-            trackOffsets = [offset for offset in trackOffsets if offset > 0]
-            
-            print(f"Detected {system} SYN file with {len(trackOffsets)} tracks")
-            
-            #Decode the SYN file using the input filename as the base for output files
-            trackPaths, combined_path = decodeSYN(f, trackOffsets, system, input_filename)
-            
-            print(f"Conversion complete for {input_filename}!")
-        #except:
-        #    ""
+            with open(synPath, "rb") as f:
+                trackOffsets = []
+                headCheck = f.read(2)
+                
+                if headCheck != b'\x02\x00': #LeapPad SYN
+                    f.seek(0)
+                    trackOffsets = BE_multiunpack.ushort(f.read(12))
+                    system = "LeapPad"
+                else: #Leapster SYN
+                    trackCount = LE_unpack.ushort(f.read(2))
+                    for track in range(trackCount):
+                        offset = LE_unpack.ushort(f.read(2))
+                        ID = BE_unpack.ushort(f.read(2))
+                        trackOffsets.append(offset)
+                    system = "Leapster"
+                
+                #Filter out any zero offsets
+                trackOffsets = [offset for offset in trackOffsets if offset > 0]
+                
+                print(f"Detected {system} SYN file with {len(trackOffsets)} tracks")
+                
+                #Decode the SYN file using the input filename as the base for output files
+                trackPaths, combined_path = decodeSYN(f, trackOffsets, system, input_filename)
+                
+                print(f"Conversion complete for {input_filename}!")
+        except:
+            ""
     
     print("\nAll files processed successfully!")
